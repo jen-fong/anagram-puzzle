10fe8671227ecc7b9539b675d8995e43bd042bb8
jen-fong <jenny@homerlearning.com>
2026-01-01 22:39:13 -0500

    Fixing the build script

--- src/factories.ts
+++ src/factories.ts
@@ -1,8 +1,13 @@
 import { AnagramPuzzle } from '@/anagram-puzzle';
 import { readTextFile } from '@/utils/file-utils';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export const DEFAULT_WORDS_FILE = 'sowpods.txt';
+// Helper to get the current directory name in ESM/CJS hybrid environments
+const _dirname =
+    typeof __dirname !== 'undefined' ? __dirname : dirname(fileURLToPath(import.meta.url));
 
+export const DEFAULT_WORDS_FILE = join(_dirname, 'sowpods.txt');
 // I chose to move the I/O out of the actual AnagramPuzzle class to decouple it
 // so all it does is handle words and the various puzzle behaviors. This will make it easier to test.
 // This can also be expanded to create a puzzle using a user submitted list of words instead of a file if needed
